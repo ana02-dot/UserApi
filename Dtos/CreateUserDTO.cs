b1002bf18e94392a5c01f00e51ae9ba67b8c313c
Chkhuto's _Hub <ana.chkhutiashvili.1@btu.edu.ge>
2025-03-14 13:44:45 +0400

    create dtos

---
+++ Dtos/CreateUserDTO.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserProfileAPI.Dtos
+{
+    public class CreateUserDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "სახელი უნდა შეიცავდეს 2-50 სიმბოლოს")]
+        [RegularExpression(@"^([ა-ჰ]+|[a-zA-Z]+)$", ErrorMessage = "სახელი უნდა შეიცავდეს მხოლოდ ქართულ ან ლათინურ ასოებს")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "გვარი უნდა შეიცავდეს 2-50 სიმბოლოს")]
+        [RegularExpression(@"^([ა-ჰ]+|[a-zA-Z]+)$", ErrorMessage = "გვარი უნდა შეიცავდეს მხოლოდ ქართულ ან ლათინურ ასოებს")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(4, ErrorMessage = "სქესი უნდა იყოს 'ქალი' ან 'კაცი'")]
+        public string GenderType { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "პირადი ნომერი უნდა შეიცავდეს 11 სიმბოლოს")]
+        public string PersonalNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 5, ErrorMessage = "მობილურის ტიპი უნდა იყოს 'ოფისი', 'სახლი' ან 'მობილური'")]
+        public string PhoneNumberType { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "ტელეფონის ნომერი უნდა შეიცავდეს მინიმუმ 4 სიმბოლოს")]
+        public string Number { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+    }
+}
---
+++ Dtos/UpdateUserDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserProfileAPI.Dtos
+{
+    public class UpdateUserDTO
+    {
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "სახელი უნდა შეიცავდეს 2-50 სიმბოლოს")]
+        [RegularExpression(@"^([ა-ჰ]+|[a-zA-Z]+)$", ErrorMessage = "სახელი უნდა შეიცავდეს მხოლოდ ქართულ ან მხოლოდ ლათინურ ასოებს")]
+        public string? FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "გვარი უნდა შეიცავდეს 2-50 სიმბოლოს")]
+        [RegularExpression(@"^([ა-ჰ]+|[a-zA-Z]+)$", ErrorMessage = "სახელი უნდა შეიცავდეს მხოლოდ ქართულ ან მხოლოდ ლათინურ ასოებს")]
+        public string? LastName { get; set; }
+
+        [StringLength(4, ErrorMessage = "სქესი უნდა იყოს 'ქალი' ან 'კაცი'")]
+        public string? GenderType { get; set; }
+
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "პირადი ნომერი უნდა შეიცავდეს 11 სიმბოლოს")]
+        public string? PersonalNumber { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime? DateOfBirth { get; set; }
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "ტელეფონის ნომერი უნდა შეიცავდეს მინიმუმ 4 სიმბოლოს")]
+        public string? Number { get; set; }
+        [StringLength(8, MinimumLength = 5, ErrorMessage = "მობილურის ტიპი უნდა იყოს 'ოფისი','სახლი' ან 'მობილური'")]
+        public string? PhoneNumberType { get; set; }
+        public int? CityId { get; set; }
+    }
+}
