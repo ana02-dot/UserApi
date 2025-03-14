c067c7d4ae3986b67c386d9e1c97e8d7173587de
Chkhuto's _Hub <ana.chkhutiashvili.1@btu.edu.ge>
2025-03-14 13:45:14 +0400

    update some lines

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -1,12 +1,13 @@
-﻿using UserProfileAPI.Models;
+﻿using UserProfileAPI.Dtos;
+using UserProfileAPI.Models;
 
 namespace UserProfileAPI.Interfaces
 {
     public interface IUserRepository
     {
         Task<UserModel> GetUserWithDetailsAsync(int id);
-        Task<UserModel> AddUserAsync(UserModel user);
-        Task<UserModel> UpdateUserAsync(int id, UserModel user);
+        Task<UserModel> AddUserAsync(CreateUserDTO userDto);
+        Task<UserModel> UpdateUserAsync(int id, UpdateUserDTO updateUserDto);
         Task<UserModel> DeleteUserAsync(int id);
 
     }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UserProfileAPI.Data;
+using UserProfileAPI.Dtos;
 using UserProfileAPI.Interfaces;
 using UserProfileAPI.Models;
 
@@ -16,57 +17,53 @@ namespace UserProfileAPI.Repositories
         public async Task<UserModel> GetUserWithDetailsAsync(int id)
         {
             return await _dbContext.Users
-                .Include(u => u.PhoneNumbers)
-                .Include(u => u.UserRelations)
-                    .ThenInclude(ur => ur.RelatedUser)
-                .Include(u => u.City)
-                .FirstOrDefaultAsync(u => u.Id == id);
+        .Include(u => u.UserRelations) // დაკავშირებული ფიზიკური პირები
+            .ThenInclude(ur => ur.RelatedUser)
+        .Include(u => u.City) // ქალაქის ინფორმაცია
+        .FirstOrDefaultAsync(u => u.Id == id);
         }
 
-        public async Task<UserModel> AddUserAsync(UserModel user)
+        public async Task<UserModel> AddUserAsync(CreateUserDTO userDto)
         {
+            var user = new UserModel
+            {
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                GenderType = userDto.GenderType,
+                PersonalNumber = userDto.PersonalNumber,
+                DateOfBirth = userDto.DateOfBirth,
+                PhoneNumberType = userDto.PhoneNumberType,
+                Number = userDto.Number,
+                CityId = userDto.CityId
+            };
+
             await _dbContext.Users.AddAsync(user);
             await _dbContext.SaveChangesAsync();
 
             return user;
         }
 
-        public async Task<UserModel> UpdateUserAsync(int id, UserModel user)
+        public async Task<UserModel> UpdateUserAsync(int id, UpdateUserDTO updateUserDto)
         {
             var existingUser = await _dbContext.Users.FindAsync(id);
+            if (existingUser == null) return null;
 
-            if (existingUser == null)
-            {
-                return null;
-            }
-           
-            existingUser.FirstName = user.FirstName;
-            existingUser.LastName = user.LastName;
-            existingUser.GenderType = user.GenderType;
-            existingUser.PersonalNumber = user.PersonalNumber;
-            existingUser.DateOfBirth = user.DateOfBirth;
-            existingUser.City = user.City;
-            existingUser.PhoneNumbers = user.PhoneNumbers;
+            if (!string.IsNullOrEmpty(updateUserDto.FirstName)) existingUser.FirstName = updateUserDto.FirstName;
+            if (!string.IsNullOrEmpty(updateUserDto.LastName)) existingUser.LastName = updateUserDto.LastName;
+            if (!string.IsNullOrEmpty(updateUserDto.GenderType)) existingUser.GenderType = updateUserDto.GenderType;
+            if (!string.IsNullOrEmpty(updateUserDto.PersonalNumber)) existingUser.PersonalNumber = updateUserDto.PersonalNumber;
+            if (updateUserDto.DateOfBirth.HasValue) existingUser.DateOfBirth = updateUserDto.DateOfBirth.Value;
+            if (!string.IsNullOrEmpty(updateUserDto.Number)) existingUser.Number = updateUserDto.Number;
 
-            
-            if (!string.IsNullOrEmpty(user.ImagePath))
-            {
-                existingUser.ImagePath = user.ImagePath;
-            }
 
             await _dbContext.SaveChangesAsync();
-
             return existingUser;
         }
 
         public async Task<UserModel> DeleteUserAsync(int id)
         {
             var user = await _dbContext.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return null;
-            }
+            if (user == null) return null;
 
             _dbContext.Users.Remove(user);
             await _dbContext.SaveChangesAsync();
