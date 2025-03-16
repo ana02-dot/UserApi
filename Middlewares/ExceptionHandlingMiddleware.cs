3cde3e7a5a070889284061f67af2ef19d798d8db
Chkhuto's _Hub <ana.chkhutiashvili.1@btu.edu.ge>
2025-03-16 16:19:45 +0400

    add Exceptionandling Middleware

---
+++ Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Text.Json;
+using System;
+
+namespace UserProfileAPI.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                _logger.LogInformation("A request has come in");
+                await _next(context);
+                _logger.LogInformation("A requst has completed successfully");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"მოხდა დაუმუშავებელი შეცდომა: {e}");
+
+                await HandleExceptionAsync(context, e);
+            }
+           
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception e)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = e switch
+            {
+                ArgumentException => (int)HttpStatusCode.BadRequest,   // 400
+                KeyNotFoundException => (int)HttpStatusCode.NotFound, // 404
+                _ => (int)HttpStatusCode.InternalServerError          // 500
+            };
+
+            context.Response.StatusCode = statusCode;
+
+            var response = new
+            {
+                StatusCode = statusCode,
+                Message = e.Message,
+                Details = e.InnerException?.Message ?? string.Empty
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(response);
+
+            return context.Response.WriteAsync(jsonResponse);
+        }
+    }
+
+}
