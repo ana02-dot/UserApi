a62e9eaf28286b4c61e5a1d3d0acf86157c6e386
Chkhuto's _Hub <ana.chkhutiashvili.1@btu.edu.ge>
2025-03-16 23:39:49 +0400

    create login DTO

---
+++ Dtos/UserLoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserProfileAPI.Dtos
+{
+    public class UserLoginDTO
+    {
+        [Required(ErrorMessage = "მომხმარებლის სახელი სავალდებულოა")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "პაროლი სავალდებულოა")]
+        public string Password { get; set; }
+    }
+}
