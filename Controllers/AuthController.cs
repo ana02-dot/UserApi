2e214cf2234c5b3c59f864a585a0442197da920a
Chkhuto's _Hub <ana.chkhutiashvili.1@btu.edu.ge>
2025-03-16 23:39:05 +0400

    add User authorization

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserProfileAPI.Data;
+using UserProfileAPI.Dtos;
+using UserProfileAPI.Interfaces;
+using UserProfileAPI.Models;
+
+namespace UserProfileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContext _dbcontext;
+        private readonly IAuthorization _authRepo;
+
+        public AuthController(DataContext dbcontext, IAuthorization authRepo)
+        {
+            _dbcontext = dbcontext;
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO userLog)
+        {
+            var user = await _dbcontext.Users
+        .FirstOrDefaultAsync(u => u.Username == userLog.Username);
+            if (user == null || userLog.Password != user.Password)
+            {
+                return Unauthorized(new { message = "მომხმარებლის სახელი ან პაროლი არასწორია" });
+            }
+
+            var token = _authRepo.GenerateToken(userLog);
+            return Ok(new
+            {
+                token,
+                user = new { id = user.Id, username = user.Username, firstName = user.FirstName, lastName = user.LastName }
+            });
+        }
+    }
+}
